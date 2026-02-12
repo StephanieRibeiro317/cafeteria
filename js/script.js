4694035e2187920be24ac2bb17a90bd2c439a38b
StephanieRibeiro317 <steh.ribeiro317@gmail.com>
2026-02-12 10:47:14 -0300

    modificacoes
    adicionado formulario de contato
    adicionado experiências

--- js/script.js
+++ js/script.js
@@ -12,3 +12,39 @@ botoes.forEach(botao => {
     modal.show();
   });
 });
+
+// pega todos que usam a classe
+// let scrollTop = document.querySelector('.scroll-top');
+
+// Precisa que a tag tenha um ID
+let scrollTop = document.getElementById('scroll-top');
+
+scrollTop.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+// document = página
+document.addEventListener('scroll', alterarScroll);
+function alterarScroll() {
+    if (window.scrollY > 100) {
+        scrollTop.classList.remove('d-none');
+    }
+    else {
+        scrollTop.classList.add('d-none');
+    }
+}
+
+document.getElementById('formContato').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const nome = document.getElementById('nome').value;
+  const resposta = document.getElementById('resposta');
+
+  resposta.innerText = `Obrigado, ${nome}! ☕ Sua mensagem foi enviada com sucesso.`;
+  resposta.style.color = "#6b3f3f";
+
+  this.reset();
+});
